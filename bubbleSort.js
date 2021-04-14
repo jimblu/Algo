f59be1eb2a05e36fc4966da26be6261f6e9f294d
jimblu <jimmy.pezp@gmail.com>
2021-04-14 21:47:50 +0200

    bubSort

---
+++ bubbleSort.js
@@ -0,0 +1,24 @@
+const bubbleSort = (arr, indexOne, indexTwo) => {
+    let swapping = true;
+    let swap = 0;
+    while (swapping) {
+      swapping = false;
+      for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i+1]) {
+          let tempVar = arr[i];
+          arr[i] = arr[i+1];
+          arr[i+1] = tempVar;
+          swap++;
+          swapping = true;
+        }
+      }
+    }
+    console.log(swap);
+    return arr;
+  }
+  
+  const a = [9, 8, 7, 6, 5, 4, 3, 2, 1] ;
+  console.log(bubbleSort(a));
+  
+  
+  
