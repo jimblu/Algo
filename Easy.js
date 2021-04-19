47bb4b5c0de99490b4439335de136ee98488a959
jimblu <jimmy.pezp@gmail.com>
2021-04-19 13:05:24 +0200

    Update Easy.js

--- Easy.js
+++ Easy.js
@@ -29,3 +29,15 @@ function findLongestWordLength(str) {
   return max;
 }
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+//Return Largest Numbers in Arrays
+function largestOfFour(arr) {
+  let Largest = [];
+  let max;
+  arr.map(subArray => {
+    max = Math.max(...subArray)
+    Largest.push(max)
+  })
+  return Largest
+}
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
